6cc7f18a8fb756736ee2b3db074903f48ea7a35c
Aditya Verma <aaadarths2357@gmail.com>
2020-11-10 17:19:40 +0530

    Added: contact class to create an object of contact with first name, last name, address, city, state, zip, phone number and email as it's attributes

--- address-book.js
+++ address-book.js
@@ -1,1 +1,25 @@
 console.log("<------------------Welcome to the Address Book System------------------>");
+
+class Contact {
+
+    constructor(...parameters) {
+        this.firstName = parameters[0];
+        this.lastName = parameters[1];
+        this.address = parameters[2];
+        this.city = parameters[3];
+        this.state = parameters[4];
+        this.zip = parameters[5];
+        this.phoneNumber = parameters[6];
+        this.email = parameters[7];
+    }
+
+    toString() {
+        return "First Name: " + this.firstName + " | Last Name: " + this.lastName + " | Address: "
+            + this.address + " | City: " + this.city + " | State: " + this.state + " | Zip: " + this.zip +
+            " | Phone Number: " + this.phoneNumber + " | Email: " + this.email + "\n";
+    }
+}
+
+let contact = new Contact("Aditya", "Verma", "8/11 LDA Colony", "Lucknow", "Uttar Pradesh", 226014, 9898808868, "aditya@gmail.com");
+console.log(contact.toString());
+
